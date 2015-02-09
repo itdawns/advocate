a87a24eb757bdef2a886dbb091edf0dd9cb2c305
Daniel Sont <dan.sont@gmail.com>
2015-02-10 00:46:00 +0700

    Components, Commands, and some Funky Objects in lib/

--- index.js
+++ index.js
@@ -1,31 +1,64 @@
-var async = require('async');
+// Script Entry Points
 
-var Loader = require('./lib/loader');
-var Assembler = require('./lib/assembler');
+var yaml = require('js-yaml');
+
+var commands = {
+
+    // node index.js render ...        
+    compile: function(options) {        
+        console.log('Compiling All Components');
+
+        require('./lib/component').loadAll(function(err, loaded) {
+            //console.log(loaded);
+            console.log(yaml.dump(loaded))
+        });
+    },
+
+    // node index.js render ...
+    render: function() {
+        console.log('Render');
+    },
+
+    // node index.js config ...
+    config: function() {
+        console.log('Config');
+    },
+}
+
+
+// Args
+var argv = require('minimist')(process.argv.slice(2));
+
+
+// Does the command exist?
+if (typeof commands[argv['_'][0]] === 'function') {
+    return commands[argv._[0]](argv);
+} else {
+    console.log('Invalid Command "' + argv['_'][0] + '"');    
+}
+
 
 var assembler = new Assembler();
 
+
+var async = require('async');
+var Assembler = require('./lib/assembler');
+
+// feed components into assembler to compile
 function loadComponents(done) {
-    Loader.load('components/', /.html$/, function templates(err, src) {
-        if (err) throw err;
-
-        assembler.compile(src);
-        done();
-    });
+    
 }
 
+// feed configs into assembler
 function loadConfigs(done) {
-    Loader.load('./config', /.yaml$/, function configs(err, src) {
-        if (err) throw err;    
-        
-        assembler.config(src);
-        done();
-    });
+
 }
 
+// assemble shit
 function assemble() {
     assembler.newConfig();
     return assembler.assemble();
 }
 
+//Load
 async.parallel([loadComponents, loadConfigs], assemble);
---
+++ think-log.md
@@ -0,0 +1,24 @@
+
+## think log of dsont.
+
+css/js dependencies libs?
+
+
+
+## Web Components
+
+Building blocks of a website
+
+ - compilation into js
+ - exports config vars
+ - 
+
+
+What they do
+
+ - HTML Markup
+ - Variables
+
+ ## Config
+
+ ## 
